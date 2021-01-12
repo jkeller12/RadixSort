8f7fceb15211a446cebd881ade7b9d032ae5298f
Jonah Keller <jkeller10@stuy.edu>
2021-01-12 12:30:49 -0500

    I downloaded mylinkedlist.java

--- Radix.java
+++ Radix.java
@@ -1,42 +1,50 @@
 public class Radix{
   public static int nth(int n, int col)
   {
-    String number = Integer.toString(n);
-    int Return_Value = Character.getNumericValue(number.charAt(col));
-    return Return_Value;
+    return Math.abs(n / (int)Math.pow(10, col) % 10);
   }
-
-/*
-get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
-nth(123,1) -> 2
-nth(-123,1) -> 2
-nth(123,2) -> 1
-nth(-123,2) -> 1
-*/
 
   public static int length(int n)
   {
-    String number = Integer.toString(n);
-    int Length_Value = number.length();
-    return Length_Value; 
+    return (int)(Math.log10(Math.abs(n))+1);
   }
-/*
-return the number of digits in n.
-length(0) -> 1
-length(15) -> 2
-length(-10) -> 2
-length(5112) -> 4
-*/
+
+
+
 
 // Requires local copy of MyLinkedList
   public static void merge(MyLinkedList original,MyLinkedList[]buckets)
   {
+    int length = buckets.length; 
+    /*
+    Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
+    This is O(buckets.length) which should be 10 when we use this later.
+    The bucket[0] list will be merged first, then the bucket[1] etc.
+    Write and thoroughly test these methods!*/
 
   }
-/*
-Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
-This is O(buckets.length) which should be 10 when we use this later.
-The bucket[0] list will be merged first, then the bucket[1] etc.
-Write and thoroughly test these methods! */
 
+
+public static void main(String[] args)
+{
+  String Return_String = "[";
+
+  Return_String = Return_String + (nth(123, 1)) + ", ";
+  Return_String = Return_String + (nth(-123, 1)) + ", ";
+  Return_String = Return_String + (nth(123, 2)) + ", ";
+  Return_String = Return_String + (nth(-123, 2)) + "]";
+  System.out.println(Return_String);
+  System.out.println("Expected Output");
+  System.out.println("[2, 2, 1, 1]");
+  System.out.println("");
+  Return_String = "[";
+  Return_String = Return_String + (length(123)) + ", ";
+  Return_String = Return_String + (length(-123)) + ", ";
+  Return_String = Return_String + (length(1203)) + ", ";
+  Return_String = Return_String + (length(-1230)) + "]";
+  System.out.println(Return_String);
+  System.out.println("Expected Output");
+  System.out.println("[3, 3, 4, 4]");
+
+}
 }
