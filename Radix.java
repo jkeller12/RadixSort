1376dd95bac3d94ca040822d3be7cb317dd432b3
Jonah Keller <jkeller10@stuy.edu>
2021-01-12 12:37:49 -0500

    Commiting not mylinked is diff

--- Radix.java
+++ Radix.java
@@ -20,35 +20,34 @@ public class Radix{
     {
       original.extend(buckets[i]);
     }
-    /*
-    Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
-    This is O(buckets.length) which should be 10 when we use this later.
-    The bucket[0] list will be merged first, then the bucket[1] etc.
-    Write and thoroughly test these methods!*/
-
   }
 
 
 public static void main(String[] args)
 {
-  String Return_String = "[";
+  for(int i = 0; i < 1; i++)
+  {
+    String Return_String = "[";
 
-  Return_String = Return_String + (nth(123, 1)) + ", ";
-  Return_String = Return_String + (nth(-123, 1)) + ", ";
-  Return_String = Return_String + (nth(123, 2)) + ", ";
-  Return_String = Return_String + (nth(-123, 2)) + "]";
-  System.out.println(Return_String);
-  System.out.println("Expected Output");
-  System.out.println("[2, 2, 1, 1]");
-  System.out.println("");
-  Return_String = "[";
-  Return_String = Return_String + (length(123)) + ", ";
-  Return_String = Return_String + (length(-123)) + ", ";
-  Return_String = Return_String + (length(1203)) + ", ";
-  Return_String = Return_String + (length(-1230)) + "]";
-  System.out.println(Return_String);
-  System.out.println("Expected Output");
-  System.out.println("[3, 3, 4, 4]");
+    Return_String = Return_String + (nth(123, 1)) + ", ";
+    Return_String = Return_String + (nth(-123, 1)) + ", ";
+    Return_String = Return_String + (nth(123, 2)) + ", ";
+    Return_String = Return_String + (nth(-123, 2)) + "]";
+    System .out.println("Function Output");
+    System.out.println(Return_String);
+    System.out.println("Expected Output");
+    System.out.println("[2, 2, 1, 1]");
+    System.out.println("");
+    Return_String = "[";
+    Return_String = Return_String + (length(123)) + ", ";
+    Return_String = Return_String + (length(-123)) + ", ";
+    Return_String = Return_String + (length(1203)) + ", ";
+    Return_String = Return_String + (length(-1230)) + "]";
+    System .out.println("Function Output");
+    System.out.println(Return_String);
+    System.out.println("Expected Output");
+    System.out.println("[3, 3, 4, 4]");
+  }
 
 }
 }
