07e9f82070008b2e733f1fe91e50135831629df4
Jonah Keller <jkeller10@stuy.edu>
2021-01-14 12:11:32 -0500

    Updated

--- Radix.java
+++ Radix.java
@@ -89,33 +89,4 @@ public class Radix{
     data.extend(Negative);
     data.extend(Positive);
   }
-
-
-/*
-public static void main(String[] args)
-{
-  for(int i = 0; i < 1; i++)
-  {
-    String Return_String = "[";
-
-    Return_String = Return_String + (nth(123, 1)) + ", ";
-    Return_String = Return_String + (nth(-123, 1)) + ", ";
-    Return_String = Return_String + (nth(123, 2)) + ", ";
-    Return_String = Return_String + (nth(-123, 2)) + "]";
-    System .out.println("Function Output");
-    System.out.println(Return_String);
-    System.out.println("Expected Output");
-    System.out.println("[2, 2, 1, 1]");
-    System.out.println("");
-    Return_String = "[";
-    Return_String = Return_String + (length(123)) + ", ";
-    Return_String = Return_String + (length(-123)) + ", ";
-    Return_String = Return_String + (length(1203)) + ", ";
-    Return_String = Return_String + (length(-1230)) + "]";
-    System .out.println("Function Output");
-    System.out.println(Return_String);
-    System.out.println("Expected Output");
-    System.out.println("[3, 3, 4, 4]");
-  }*/
-
 }
